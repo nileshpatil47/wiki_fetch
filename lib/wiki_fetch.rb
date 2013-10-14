175ce2679a88875913cede602b3e254070f87935
nileshpatil47 <nileshpatil47@yahoo.com>
2013-10-14 11:47:14 +0530

    search for suggestions

--- lib/wiki_fetch.rb
+++ lib/wiki_fetch.rb
@@ -6,8 +6,18 @@ class Wiki
 		@@agent = Mechanize.new
   end
 
-	def search(name, format = "json" )
+	def search_form_title(name, format = "json" )
 		url = "http://en.wikipedia.org/w/api.php?format=#{format}&action=query&prop=extracts&exsentences=10&titles=#{name}"
+		begin
+			page_data = @@agent.get url
+			return page_data.body
+		rescue => e
+			e.message
+		end
+	end
+	
+	def suggestions(name)
+		url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=#{name}&limit=15&namespace=0&format=json"
 		begin
 			page_data = @@agent.get url
 			return page_data.body
