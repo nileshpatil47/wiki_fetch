93c76f3b28613d189483ff36f3606f4048c23863
nileshpatil47 <nileshpatil47@yahoo.com>
2013-10-14 11:17:05 +0530

    lib file changes

--- lib/wiki_fetch.rb
+++ lib/wiki_fetch.rb
@@ -1,18 +1,17 @@
 require "wiki_fetch/version"
 require 'mechanize'
 
-module WikiFetch
-  def self.ipsum
-  	"Hi we will work on wiki data"
+class Wiki
+	def initialize()
+		@@agent = Mechanize.new
   end
-  class WikiFetch
-  	def initialize()
-  		@@agent = Mechanize.new
-    end
 
-  	def self.fetch(url)
-  		page_data = @@agent.get url
-  		page_data.body
-  	end
-  end
+	def fetch(url)
+		begin
+			page_data = @@agent.get url
+			page_data.body
+		rescue => e
+			e.message
+		end
+	end
 end
